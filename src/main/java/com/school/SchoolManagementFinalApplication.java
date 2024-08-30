f26b830b3dee246736162ae28fcae1374c4a7fbf
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-30 23:53:23 +0400

    modelMapper

--- src/main/java/com/school/SchoolManagementFinalApplication.java
+++ src/main/java/com/school/SchoolManagementFinalApplication.java
@@ -1,7 +1,9 @@
 package com.school;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SchoolManagementFinalApplication {
@@ -10,4 +12,8 @@ public class SchoolManagementFinalApplication {
         SpringApplication.run(SchoolManagementFinalApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper mapper(){
+        return new ModelMapper();
+    }
 }
