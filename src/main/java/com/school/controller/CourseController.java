ba96b2de751fa0ebf85727b4537283fdcc4a4187
fai7h01 <l.varsimashvil10@gmail.com>
2024-08-31 23:00:00 +0400

    course controller create

--- src/main/java/com/school/controller/CourseController.java
+++ src/main/java/com/school/controller/CourseController.java
@@ -1,6 +1,7 @@
 package com.school.controller;
 
 import com.school.dto.CourseDTO;
+import com.school.entity.Course;
 import com.school.service.CourseService;
 import com.school.service.UserService;
 import org.springframework.stereotype.Controller;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/course")
@@ -32,13 +34,15 @@ public class CourseController {
     }
 
     @PostMapping("/create")
-    public String insertCourse(@ModelAttribute("course") CourseDTO courseDTO, BindingResult bindingResult, Model model){
+    public String insertCourse(@ModelAttribute("course") CourseDTO courseDTO, BindingResult bindingResult,
+                               RedirectAttributes redirectAttributes, Model model){
         if (bindingResult.hasErrors()){
             model.addAttribute("courses", courseService.findAll());
             model.addAttribute("managers", userService.listAllByRole("Manager"));
             return "/course/course-create";
         }
-        courseService.save(courseDTO);
+        CourseDTO saved = courseService.save(courseDTO);
+        redirectAttributes.addFlashAttribute("success", saved.getName() + " is successfully added to courses");
         return "redirect:/course/create";
     }
 }
